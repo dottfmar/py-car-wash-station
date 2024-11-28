f56d330410b0ebd518e14fb6f5d6df7199c0dafe
Mariana Lishchynska <lishchynska.m@gmail.com>
2024-11-28 14:38:05 +0200

    'Fixed'

--- app/main.py
+++ app/main.py
@@ -1,33 +1,39 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int, brand: str) -> None:
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        if car.clean_mark >= self.clean_power:
-            return 0
-
-        price = (car.comfort_class * (self.clean_power
-                                      - car.clean_mark) * self.average_rating
-                 / self.distance_from_city_center)
-        return round(price, 1)
+        return round(
+            (car.comfort_class
+             * (self.clean_power
+                - car.clean_mark)
+             * self.average_rating
+             / self.distance_from_city_center),
+            1
+        )
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0.0
         for car in cars:
             wash_price = self.calculate_washing_price(car)
@@ -36,8 +42,7 @@ class CarWashStation:
                 income += wash_price
         return round(income, 1)
 
-    def rate_service(self, new_rating: float) -> None:
-        total_rating = self.average_rating * self.count_of_ratings
-        total_rating += new_rating
+    def rate_service(self, rating: float) -> None:
+        total_rating = self.average_rating * self.count_of_ratings + rating
         self.count_of_ratings += 1
         self.average_rating = round(total_rating / self.count_of_ratings, 1)
